948c06b68bcf9e1d8bf2ca72d71ad784118c636f
AndriiLiubarskyi <heinzanlu@gmail.com>
2021-07-24 18:39:46 +0300

    hw-react-02phonebook-ver02

--- src/components/Phonebook/ContactList.js
+++ src/components/Phonebook/ContactList.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ContactItem from './ContactItem';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <div>
-    <h2>Contacts</h2>
-    <ul>
-      {contacts.map(({ id, name, number, }) => (
-        <li key={id}>
-          <p>{name}</p>
-          <p>{number}</p>
-          
-          <button onClick={() => onDeleteContact(id)}>Удалить</button>
-        </li>
-      ))}
-    </ul>
-  </div>
+const ContactList = ({contacts, onDeleteContact}) => (
+  <ul>
+    {contacts.map(({ id, name, number }) => (
+      <ContactItem
+        key={id}
+        id={id}
+        name={name}
+        number={number}
+        onDeleteContact={onDeleteContact}
+      />
+    ))}
+  </ul>
 );
+
+ContactList.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func,
+};
 
 export default ContactList;
